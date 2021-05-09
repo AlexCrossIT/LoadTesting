b417757bb821a0aa5af7e9aa44f3f0cf6bb55ad2
AlexCross <AlexCrossIT@live.ru>
2021-05-09 20:19:41 +0300

    Rename servlets

--- loadtesting/src/main/java/com/multitreading/loadtesting/servlets/LoadTestingServlet.java
+++ loadtesting/src/main/java/com/multitreading/loadtesting/servlets/LoadTestingServlet.java
@@ -1,20 +1,24 @@
 package com.multitreading.loadtesting.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MainPage extends HttpServlet{
+public class LoadTestingServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		req.getRequestDispatcher("index.jsp").forward(req, resp);
+		PrintWriter out = resp.getWriter();
+		out.println("Load testing is started");
+		
+		//super.doPost(req, resp);
 		
 	}
 
--- loadtesting/src/main/java/com/multitreading/loadtesting/servlets/MainPageServlet.java
+++ loadtesting/src/main/java/com/multitreading/loadtesting/servlets/MainPageServlet.java
@@ -1,24 +1,20 @@
 package com.multitreading.loadtesting.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoadTesting extends HttpServlet{
+public class MainPageServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		PrintWriter out = resp.getWriter();
-		out.println("Load testing is started");
-		
-		//super.doPost(req, resp);
+		req.getRequestDispatcher("index.jsp").forward(req, resp);
 		
 	}
 
