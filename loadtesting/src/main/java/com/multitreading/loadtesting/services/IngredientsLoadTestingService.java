e069135f9ef1e22acc88ae4127a787cdaa47e4f3
AlexCross <AlexCrossIT@live.ru>
2021-05-12 23:15:22 +0300

    Fixed

--- loadtesting/src/main/java/com/multitreading/loadtesting/services/IngredientsLoadTestingService.java
+++ loadtesting/src/main/java/com/multitreading/loadtesting/services/IngredientsLoadTestingService.java
@@ -4,27 +4,26 @@ package com.multitreading.loadtesting.services;
 import com.multitreading.loadtesting.interfaces.IngredientsLoadTestingServiceBehavior;
 import com.multitreading.loadtesting.threads.IngredientsThread;
 
-
 public class IngredientsLoadTestingService implements IngredientsLoadTestingServiceBehavior{
 
 	@Override
 	public void createIngredients(String url, int threadsQuantity, int ingredientsQuantity) {
 		
-		int ingredientsQuantityPerThread = ingredientsQuantity / threadsQuantity;
+		int ingredientQuantityPerThread = ingredientsQuantity / threadsQuantity;
 		
 		for(int i = 1; i <= threadsQuantity; i++) {
 			
 			if(i == threadsQuantity) {
 				
-				int lastThreadIngredientsQuantity = ingredientsQuantity - ingredientsQuantityPerThread * (threadsQuantity - 1);
+				int lastThreadIngredientQuantity = ingredientsQuantity - ingredientQuantityPerThread * (threadsQuantity - 1); 
 				
-				Thread thread = new Thread(new IngredientsThread(url, lastThreadIngredientsQuantity));
-				thread.run();
+				Thread thread = new Thread(new IngredientsThread(url, lastThreadIngredientQuantity));
+				thread.start();
 				
 			} else {
 				
-				Thread thread = new Thread(new IngredientsThread(url, ingredientsQuantityPerThread));
-				thread.run();
+				Thread thread = new Thread(new IngredientsThread(url, ingredientQuantityPerThread));
+				thread.start();
 				
 			}
 			
