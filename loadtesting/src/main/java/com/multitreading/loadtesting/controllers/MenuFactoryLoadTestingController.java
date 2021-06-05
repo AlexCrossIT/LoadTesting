d5dcde09213c279baf1ae0d18611852b5c331bc4
AlexCross <AlexCrossIT@live.ru>
2021-06-05 16:03:52 +0300

    Removed old code

--- loadtesting/src/main/java/com/multitreading/loadtesting/controllers/MenuFactoryLoadTestingController.java
+++ loadtesting/src/main/java/com/multitreading/loadtesting/controllers/MenuFactoryLoadTestingController.java
@@ -1,9 +1,7 @@
 package com.multitreading.loadtesting.controllers;
 
 import com.multitreading.loadtesting.annotations.Inject;
-import com.multitreading.loadtesting.factories.BeanFactory;
 import com.multitreading.loadtesting.interfaces.LoadTestingServiceBehavior;
-import com.multitreading.loadtesting.singletones.MenuFactoryProperties;
 
 public class MenuFactoryLoadTestingController extends Controller{
 	
--- loadtesting/src/main/java/com/multitreading/loadtesting/servlets/LoadTestingServlet.java
+++ loadtesting/src/main/java/com/multitreading/loadtesting/servlets/LoadTestingServlet.java
@@ -2,18 +2,12 @@ package com.multitreading.loadtesting.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.annotation.Annotation;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.multitreading.loadtesting.annotations.Inject;
-import com.multitreading.loadtesting.contexts.ApplicationContext;
-import com.multitreading.loadtesting.factories.BeanFactory;
-import com.multitreading.loadtesting.interfaces.ApplicationContextBehavior;
-import com.multitreading.loadtesting.interfaces.BeanFactoryBehavior;
 import com.multitreading.loadtesting.interfaces.LoadTestingControllerBehavior;
 
 public class LoadTestingServlet extends LoadTestingHttpServlet{
@@ -22,19 +16,6 @@ public class LoadTestingServlet extends LoadTestingHttpServlet{
 
 	@Inject private LoadTestingControllerBehavior loadTestingControllerBehavior;
 	
-	/*
-	public void init() throws ServletException {
-
-		ApplicationContextBehavior applicationContextBehavior = new ApplicationContext();
-		BeanFactoryBehavior beanFactoryBehavior = new BeanFactory(applicationContextBehavior);
-		applicationContextBehavior.setBeanFactory(beanFactoryBehavior);
-		
-		Annotation[] annotations = new Annotation[0];
-		
-		this.loadTestingControllerBehavior = applicationContextBehavior.getBean(LoadTestingControllerBehavior.class, annotations);
-		
-		
-    }*/
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
