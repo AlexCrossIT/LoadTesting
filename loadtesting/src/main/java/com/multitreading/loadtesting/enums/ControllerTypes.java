c699326e86433c4d1794e094c6a0967d4aa9b12b
AlexCross <AlexCrossIT@live.ru>
2021-06-05 16:01:00 +0300

    Removed useless elements

--- loadtesting/src/main/java/com/multitreading/loadtesting/enums/ControllerTypes.java
+++
@@ -1,7 +0,0 @@
-package com.multitreading.loadtesting.enums;
-
-public enum ControllerTypes {
-
-	MENU_FACTORY_LOAD_TESTING_CONTROLLER;
-	
-}
--- loadtesting/src/main/java/com/multitreading/loadtesting/enums/ServiceTypes.java
+++
@@ -1,7 +0,0 @@
-package com.multitreading.loadtesting.enums;
-
-public enum ServiceTypes {
-
-	MENU_FACTORY_LOAD_TESTING_SERVICE;
-	
-}
